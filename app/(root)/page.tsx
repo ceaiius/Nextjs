730ea652c73ca8b71f2fcbef7e45a81d5e09cac9
Nika Mamaladze <n_mamaladze@cu.edu.ge>
2025-02-23 17:50:57 +0400

    feat: add home page and setup sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,39 @@
-export default function Home() {
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({searchParams} : {searchParams: Promise<{query: string}>}) {
+  const query = (await searchParams).query;
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: {_id: 1, name: 'John Doe'},
+    _id: 1,
+    description: 'A platform that helps you find the best online courses',
+    image: 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg',
+    category: 'Robots',
+    title: 'We Robots'
+  }];
   return (
     <>
-    <h1 className="text-3xl text-white">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">Pitch your Startup, <br /> Connect With Entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.</p>
+        <SearchForm query={query}/>
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "Latest Pitches"}  
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+                <StartupCard key={post?._id} post={post}/>
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
     </>
   );
 }
