661a2886459b28dd9cddab6d1a383644b7728d93
Nika Mamaladze <n_mamaladze@cu.edu.ge>
2025-02-23 18:50:54 +0400

    feat: add sanity schemas and fetch data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,12 @@
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupCardType } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({searchParams} : {searchParams: Promise<{query: string}>}) {
   const query = (await searchParams).query;
-  const posts = [{
-    _createdAt: new Date(),
-    views: 55,
-    author: {_id: 1, name: 'John Doe'},
-    _id: 1,
-    description: 'A platform that helps you find the best online courses',
-    image: 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg',
-    category: 'Robots',
-    title: 'We Robots'
-  }];
+  const posts = await client.fetch(STARTUPS_QUERY);
+  
   return (
     <>
       <section className="pink_container">
@@ -26,7 +20,7 @@ export default async function Home({searchParams} : {searchParams: Promise<{quer
         </p>
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType, index: number) => (
+            posts.map((post: StartupCardType) => (
                 <StartupCard key={post?._id} post={post}/>
             ))
           ) : (
